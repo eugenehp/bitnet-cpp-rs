5abc8291742b8f4ee6370d95a26f4bb5e2050147
Eugene Hauptmann <eugene@reactivelions.com>
2024-11-17 00:01:49 -0500

    git reset --hard origin/HEAD

--- bitnet-cpp-sys/patches/clean.sh
+++ bitnet-cpp-sys/patches/clean.sh
@@ -4,10 +4,10 @@ PATCHES="$( cd "$( dirname "$0" )" && pwd )"
 DIR="$PATCHES/.."
 
 cd "$DIR/bitnet/3rdparty/llama.cpp"
-git reset --hard
+git reset --hard origin/HEAD
 git clean -f -d
 
 cd "$DIR/bitnet"
-git reset --hard
+git reset --hard origin/HEAD
 git clean -f -d
 
