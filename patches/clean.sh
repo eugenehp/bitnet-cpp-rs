438a6603e051560ec0ea3b9b7bd427dc7eef574e
Eugene Hauptmann <eugene@reactivelions.com>
2024-11-16 21:43:53 -0500

    added clean.sh

---
+++ patches/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+cd "$DIR/../bitnet-cpp-sys/bitnet/3rdparty/llama.cpp"
+git reset --hard
+git clean -f -d
+
+cd "$DIR/../bitnet-cpp-sys/bitnet"
+git reset --hard
+git clean -f -d
+
