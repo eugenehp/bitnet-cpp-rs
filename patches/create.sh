1e153001968bd8247e971946c64f2dd06ed45e9e
Eugene Hauptmann <eugene@reactivelions.com>
2024-11-16 21:20:58 -0500

    removed generated patches

--- patches/create.sh
+++ patches/create.sh
@@ -3,9 +3,10 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cd "$DIR/../bitnet-cpp-sys/bitnet"
+git add -N * # needed because git is not tracking created files
 git diff > "$DIR/bitnet.patch"
-diff /dev/null include/bitnet-lut-kernels.h > "$DIR/bitnet-lut-kernels.patch"
-diff /dev/null include/kernel_config.ini > "$DIR/kernel_config.patch"
+# diff /dev/null include/bitnet-lut-kernels.h > "$DIR/bitnet-lut-kernels.patch"
+# diff /dev/null include/kernel_config.ini > "$DIR/kernel_config.patch"
 
 cd "$DIR/../bitnet-cpp-sys/bitnet/3rdparty/llama.cpp"
 git diff > "$DIR/llama.cpp.patch"
