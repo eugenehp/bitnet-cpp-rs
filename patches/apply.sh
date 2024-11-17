5d9e1ba734fd36bbedbc7be841d5b67ca00c3a40
Eugene Hauptmann <eugene@reactivelions.com>
2024-11-16 21:35:27 -0500

    working apply.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -4,16 +4,17 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cd "$DIR/../bitnet-cpp-sys/bitnet"
 git reset --hard # reset state of the git submodule
+git clean -f -d # cleans untracked files
+git apply --whitespace=fix "$DIR/bitnet.patch"
 
-touch include/kernel_config.ini
-patch include/kernel_config.ini "$DIR/kernel_config.patch"
+# touch include/kernel_config.ini
+# patch include/kernel_config.ini "$DIR/kernel_config.patch"
 
-touch include/bitnet-lut-kernels.h
-patch include/bitnet-lut-kernels.h "$DIR/bitnet-lut-kernels.patch"
+# touch include/bitnet-lut-kernels.h
+# patch include/bitnet-lut-kernels.h "$DIR/bitnet-lut-kernels.patch"
 
-git apply "$DIR/bitnet.patch"
-# git apply "$DIR/bitnet-lut-kernels.patch"
-# git apply "$DIR/kernel_config.patch"
 
-# cd "$DIR/../bitnet-cpp-sys/bitnet/3rdparty/llama.cpp"
-# git apply "$DIR/llama.cpp.patch"
+cd "$DIR/../bitnet-cpp-sys/bitnet/3rdparty/llama.cpp"
+git reset --hard # reset state of the git submodule
+git clean -f -d # cleans untracked files
+git apply "$DIR/llama.cpp.patch"
